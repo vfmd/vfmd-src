65f40fe9646a0981e5549aef38b504579be15a8b
Roopesh Chander <roop@forwardbias.in>
2014-04-03 13:31:26 +0530

    cmd: Support -t for tree output

--- cmd/main.cpp
+++ cmd/main.cpp
@@ -8,7 +8,6 @@
 #include "vfmdoutputdevice.h"
 
 #define BUFFER_SIZE 1024
-// #define OUTPUT_TREE
 
 int main(int argc, char *argv[])
 {
@@ -16,9 +15,27 @@ int main(int argc, char *argv[])
     VfmdScopedPointer<VfmdElementRegistry> registry(VfmdElementRegistry::createRegistryWithDefaultElements(&linkRefMap));
     VfmdDocument document(registry.data());
 
+    char *fileName = 0;
     FILE *inputFile = 0;
+    bool isTreeFormatOutput = false;
+
     if (argc >= 2) {
-        char *fileName = argv[1];
+        fileName = argv[1];
+        if (fileName[0] == '-') {
+            if (strcmp(fileName, "-t") == 0) {
+                isTreeFormatOutput = true;
+            } else {
+                fprintf(stderr, "Unknown option: %s (Only -t is supported)\n", fileName);
+                return -1;
+            }
+            if (argc >= 3) {
+                fileName = argv[2];
+            } else {
+                fileName = 0;
+            }
+        }
+    }
+    if (fileName != 0) {
         inputFile = fopen(fileName , "rb");
         if (inputFile == NULL) {
             fputs("Cannot open input file", stderr);
@@ -37,16 +54,18 @@ int main(int argc, char *argv[])
 
     VfmdElementTreeNode *parseTree = document.end();
     VfmdConsoleOutputDevice console;
-#ifdef OUTPUT_TREE
-    parseTree->renderSequence(VfmdConstants::TREE_FORMAT,
-                              (VfmdConstants::TREE_RENDER_INCLUDES_TEXT),
-                              &console);
-#else
-    parseTree->renderSequence(VfmdConstants::HTML_FORMAT,
-                              (VfmdConstants::HTML_INDENT_ELEMENT_CONTENTS |
-                               VfmdConstants::HTML_RENDER_VOID_TAGS_AS_SELF_CLOSING_TAGS),
-                              &console);
-#endif
+
+    if (isTreeFormatOutput) {
+        parseTree->renderSequence(VfmdConstants::TREE_FORMAT,
+                                  (VfmdConstants::TREE_RENDER_INCLUDES_TEXT),
+                                  &console);
+    } else {
+        parseTree->renderSequence(VfmdConstants::HTML_FORMAT,
+                                  (VfmdConstants::HTML_INDENT_ELEMENT_CONTENTS |
+                                   VfmdConstants::HTML_RENDER_VOID_TAGS_AS_SELF_CLOSING_TAGS),
+                                  &console);
+    }
+
     VfmdElementTreeNode::freeSubtreeSequence(parseTree);
 
     return 0;
