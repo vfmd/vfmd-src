f83f5bfb6ffce677b86439067b67e6e6df99c3db
Roopesh Chander <roop@forwardbias.in>
2014-04-12 18:44:02 +0530

    NullBlock: Remove unnecessary blank lines in the code

--- src/blockelements/nullblockhandler.h
+++ src/blockelements/nullblockhandler.h
@@ -7,22 +7,16 @@ class NullBlockHandler : public VfmdBlockElementHandler
 {
 public:
     NullBlockHandler() { }
-
     virtual void createChildSequence(VfmdInputLineSequence *lineSequence, const VfmdLine *firstLine, const VfmdLine *nextLine) const;
-
     virtual const char *description() const { return "null-block"; }
 };
 
 class NullBlockLineSequence : public VfmdBlockLineSequence {
 public:
     NullBlockLineSequence(const VfmdInputLineSequence *parent);
-
     virtual int elementType() const { return VfmdConstants::NULL_BLOCK_ELEMENT; }
-
     virtual void processBlockLine(const VfmdLine *currentLine, const VfmdLine *nextLine);
-
     virtual bool isEndOfBlock(const VfmdLine *currentLine, const VfmdLine *nextLine) const;
-
     virtual VfmdElementTreeNode* endBlock();
 };
 
